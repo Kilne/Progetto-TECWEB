daa535bac72dad184687a1fea1932ed97e653295
Kilne <46303813+Kilne@users.noreply.github.com>
2021-07-20 19:37:24 +0200

    Aggiunto Accordion in Testing.html, CSS da fare per i colori,rimosso HTTPS da AppMain.py

--- AppMain.py
+++ AppMain.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
@@ -16,4 +17,4 @@ def test():
 
 # Page started in debug modify after
 if __name__ == "__main__":
-    app.run('127.0.0.1', debug=True, port=5000, ssl_context='adhoc')
+    app.run(debug="true")
