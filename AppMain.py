491f4631282a843a9b5e8893df5cd7d275ae0c1f
Kilne <46303813+Kilne@users.noreply.github.com>
2021-07-26 16:21:51 +0200

    Testing del DB

--- AppMain.py
+++ AppMain.py
@@ -1,12 +1,11 @@
-import flask
-from flask import render_template
-from flask_pymongo import *
+from flask import render_template, Flask
+from pymongo import MongoClient
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 # connecting DB
 app.config['MONGO_URI'] = "mongodb://127.0.0.1:5000/app_db"
-mongodb_client = PyMongo(app)
+mongodb_client = MongoClient(app)
 db = mongodb_client.db
 
 
@@ -20,20 +19,6 @@ def home():
 @app.route("/test", methods=['GET', 'POST'])
 def test():
     return render_template('Testing.html')
-
-
-# db test
-@app.route("/add", methods=["GET"])
-def add():
-    db.tools.insert_one({'title: this is a test', 'body: the body of the test'})
-    return flask.jsonify(message="success")
-
-
-# @TODO:testare il database
-@app.route("/show")
-def show():
-    result = db.tools.find()
-    return flask.jsonify([tool for tool in result])
 
 
 # Page started in debug modify after
