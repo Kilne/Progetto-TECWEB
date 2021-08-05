4106be39c7909913d7f6ba70f1f9daf16deb0d9a
Kilne <46303813+Kilne@users.noreply.github.com>
2021-08-05 19:35:51 +0200

    - Service worker first implementation

--- static/PWA FILES/sw.js
+++ static/PWA FILES/sw.js
@@ -1,18 +1,23 @@
-// @TODO: FINIRE IL SERVICE WORKER
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register("static/PWA FILES/sw.js")
-        .then(serviceWorker => {
-            console.log("Service Worker registered: ", serviceWorker);
-        })
-        .catch(error => {
-            console.error("Error registering the Service Worker: ", error);
-        });
-    self.addEventListener("install", event => {
-        console.log("Service Worker installing.");
-    });
+let cacheName = "TAPP_CACHE"
+let filesToCache = [
+    "static/JS/Main.js",
+    "static/CSS/Style.css",
+    "templates/Main.html",
+    "templates/Testing.html"
+];
 
-    self.addEventListener("activate", event => {
-        console.log("Service Worker activating.");
-    });
-}
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request)
+        })
+    );
+});
