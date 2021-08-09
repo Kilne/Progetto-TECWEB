56e11c282970be8fde725942d1246360505462fb
Kilne <46303813+Kilne@users.noreply.github.com>
2021-08-09 20:14:49 +0200

    Adattameto dei template jinja2 da Main.htmla Testing.html, aggiunta dell'escaping in AppMain.py, cambio colori background in Style.css salavatggio file run e interprete definitivo fuori progetto (portatile)

--- AppMain.py
+++ AppMain.py
@@ -1,11 +1,17 @@
 from flask import render_template, Flask
 from flask_cors import CORS
+from jinja2 import select_autoescape, Environment
 
 app = Flask(__name__)
 CORS(app)
+# jinja2 autoescape
+env = Environment(autoescape=select_autoescape(
+    enabled_extensions=('html', 'xml'),
+    default_for_string=True,
+))
 
 
-# Main session page
+# Main page
 @app.route("/")
 def home():
     return render_template('Main.html')
