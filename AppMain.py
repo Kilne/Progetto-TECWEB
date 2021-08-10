98a16fd95a608ed51f3c3d924c8d39199fe4b2ae
Kilne <46303813+Kilne@users.noreply.github.com>
2021-08-10 19:25:27 +0200

    Added mongoDB Atlas conenction string  connection works

--- AppMain.py
+++ AppMain.py
@@ -1,6 +1,7 @@
 from flask import render_template, Flask
 from flask_cors import CORS
 from jinja2 import select_autoescape, Environment
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +10,19 @@ env = Environment(autoescape=select_autoescape(
     enabled_extensions=('html', 'xml'),
     default_for_string=True,
 ))
+
+# connessione funziona anche se senza SSL , capire perchè stampa due volte l'ID
+# MongoDB atlas connection client
+clientDB = MongoClient("mongodb+srv://Luca:WliL3VbEqdbl5VAX@clusterprogetto.0ocor.mongodb.net/ClusterProgetto"
+                       "?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs='CERT_NONE')
+dbObject = clientDB.ClusterProgetto
+collectionTest = dbObject.CollTest
+post = {
+    "author": "Luca",
+    "test": "yes"
+}
+id_inserted = collectionTest.insert_one(post).inserted_id
+print(id_inserted)
 
 
 # Main page
