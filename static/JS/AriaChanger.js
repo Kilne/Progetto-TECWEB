8d59582868cbbe45791f629613a33c282da3b247
Kilne <46303813+Kilne@users.noreply.github.com>
2021-08-17 19:02:25 +0200

    Completed aria changer file, it correctly changes the active class in the navbar for now

--- static/JS/AriaChanger.js
+++ static/JS/AriaChanger.js
@@ -1,4 +1,16 @@
+// Aspetto il documento che sia pronto
 $(document).ready(function () {
+    // Prendo l'indirizzo dal browser
     let currentAddress = window.location.href;
-    //@todo continuare con jQUERy
+    // Selezioni tutti gli elementi <a> con class nav-link e ciclo con .each(funzione(indice,elemento))
+    $("a.nav-link").each(function (idx, ele) {
+        /*Dato che Jinja2/flask con url_for crea il link al momento del caricamento
+        lo conforto con l'indirizzo corrente*/
+        if (ele.href === currentAddress) {
+            // Se i due indirizzi corrispondono gli do la classe active
+            ele.classList.add("active");
+            // Inoltre gli assegno l'aria current come da linee guida W3C
+            $(this).attr("aria-current", "page");
+        }
+    })
 })
