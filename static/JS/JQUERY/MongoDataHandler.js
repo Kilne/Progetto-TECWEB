bdc04f47fd2296126824902140fb75b03e3993e6
Kilne <luca.maiuri86@gmail.com>
2021-09-30 16:41:51 +0200

    struggle with fetch and jequery
    Former-commit-id: dd83e0f582c19abdbe9fe0873a65762a8bdbb22d

--- static/JS/JQUERY/MongoDataHandler.js
+++ static/JS/JQUERY/MongoDataHandler.js
@@ -1,6 +1,1 @@
-async function post_data(username) {
-    //@todo:devi usare la fetch lascia perdere jquery
-    const db_data = await fetch("/db/" + username);
 
-    return db_data.json()
-}
--- static/JS/JSMAINS/TESTPAGE/TestMain.js
+++
@@ -1,8 +0,0 @@
-//@todo:continua a darmi promesse.....
-async function myDisplay() {
-    return await post_data("UserTest")
-}
-
-let dbs_data = await myDisplay()
-
-console.log(dbs_data)
