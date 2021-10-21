d15539b662e1a5e8246fc7459672ac94d5af0a6b
Christian Porritiello <83812141+ChristianPorr@users.noreply.github.com>
2021-10-21 11:17:49 +0200

    Registrazione ok Login ok

--- AppMain.py
+++ AppMain.py
@@ -1,13 +1,13 @@
-import time
-
 from flask import render_template, Flask, request, json, abort, flash, session, redirect, url_for
+from datetime import timedelta
 # from flask_cors import cross_origin
 from jinja2 import select_autoescape, Environment
 # from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 
 app = Flask(__name__)
-app.secret_key = 'Afaccrocazz'
+app.secret_key = 'A_facc_ro_cazz'
+app.permanent_session_lifetime = timedelta(hours=1)
 # jinja2 autoescape
 env = Environment(autoescape=select_autoescape(
     enabled_extensions=('html', 'xml'),
@@ -24,8 +24,9 @@ collection = db.UsersInfo
 @app.route("/")
 def home():
     if 'username' in session:
-        return flash('You are logged in as ' + session['username'])
-    return render_template('Main.html')
+        return render_template('Main.html'), flash('You are logged in as ' + session['username'], 'info')
+    else:
+        return render_template('Main.html'), flash('You are not logged in', 'danger')
 
 
 @app.route("/signup/", methods=['GET', 'POST'])
@@ -36,38 +37,43 @@ def signUp():
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
         # risolto, togliendo le parentesi quadrate da 'POST' e' andato .-.
-        if db.UsersInfo.find_one({'email': _email}):  # qui non va bene non c'è controllo per la registrazione
-            flash('Your credential already exist, try again', 'error')
+        if _name and _surname and _email and _password:
+            if db.UsersInfo.find_one({'email': _email}):
+                flash('Your credential already exist, try again', 'danger')
+            else:
+                info = {
+                    "name": _name,
+                    "surname": _surname,
+                    "email": _email,
+                    "password": _password
+                }
+                save_info = collection.insert_one(info)
+                save_info
+                return redirect(url_for('login'))
         else:
-            info = {
-                "name": _name,
-                "surname": _surname,
-                "email": _email,
-                "password": _password
-            }
-            save_info = collection.insert_one(info)
-            save_info
-            return redirect(url_for('login'))
+            flash('Insert all the camps', 'danger')
     return render_template('Sign_Up.html')
-
-
-# min 32 vedi il post per il login
 
 
 @app.route("/signin/", methods=['POST', 'GET'])
 def login():
+    if 'username' in session:
+        flash('You are already logged in!', 'danger')
+        return redirect(url_for('home'))
     if request.method == 'POST':
-        render_template('Login.html')
+        _name = request.form['inputName']
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
-        existing_user = collection.find_one(
-            {"_id": 0, "name": 0, "surname": 0, "email": _email, "password": _password})
-        if existing_user is None:  # se risolvi il precedente risolvi anche questo problema
-            flash('Your credential are wrong, try again')
-        else:
-            _username = collection.find({}, {"_id": 0, "name": 1, "surname": 0, "email": 0, "password": 0})
-            session['username'] = _username
+        existing_user = collection.find_one({"name": _name, "email": _email, "password": _password})
+        if existing_user is None:
+            flash('Your credential are wrong, try again', 'danger')
+        elif _name and _email and _password:
+            session.permanent = True
+            _username = _name
+            session['username'] = _name
             return redirect(url_for('home'))
+        else:
+            flash('Insert all camps please', 'info')
     return render_template('Login.html')
 
 
