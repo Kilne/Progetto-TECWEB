926da2cb009d294dc345f6ac81c1a8589defee8e
Kilne <luca.maiuri86@gmail.com>
2021-10-31 18:38:22 +0100

    Bug fixes e completamento funzioni mongo handler e test
    Former-commit-id: 8e89aaf88da573668a4a76982ca22e0761524620

--- AppMain.py
+++ AppMain.py
@@ -1,7 +1,10 @@
-from flask import render_template, Flask, request, flash, session, redirect, url_for
+from bson import ObjectId
+from flask import render_template, Flask, request, flash, session, redirect, url_for, abort, jsonify
+from flask_cors import cross_origin
 from jinja2 import select_autoescape, Environment
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
+# https://en.wikipedia.org/wiki/List_of_HTTP_status_codes#5xx_server_errors
 # Flask initialization
 app = Flask(__name__)
 app.config.from_pyfile('flaskConfig.py')
@@ -13,8 +16,6 @@ env = Environment(autoescape=select_autoescape(
 ))
 
 client = MongoClient(app.config["DATABASE_URI"], ssl=True, ssl_cert_reqs='CERT_NONE')
-db = client.Users
-collection = db.UsersInfo
 
 
 # Main page
@@ -32,6 +33,8 @@ def home():
 @app.route("/signup/", methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
+        db = client.Users
+        collection = db.UsersInfo
         _name = request.form['inputName']
         _surname = request.form['inputSurname']
         _email = request.form['inputEmail']
@@ -54,12 +57,14 @@ def signup():
                 save_info = collection.insert_one(info)
                 return redirect(url_for('login'))
         else:
-            flash('Insert all the camps', 'danger')
+            flash('All fields are required', 'danger')
     return render_template('Sign_Up.html')
 
 
 @app.route("/signin/", methods=['POST', 'GET'])
 def login():
+    db = client.Users
+    collection = db.UsersInfo
     if 'username' in session:
         flash('You are already logged in!', 'danger')
         return redirect(url_for('home'))
@@ -98,6 +103,124 @@ def user():
     return render_template('UserProject.html')
 
 
+# Mongo user collection validation
+def validate_user():
+    db = client.UserProjects
+    try:
+        db.validate_collection(session.get("username"))
+        return db
+    except errors.OperationFailure:
+        return False
+
+
+# Mongo user data collection getter
+@app.route("/db/getALL/", methods=["GET", "POST"])
+@cross_origin()
+def get_documents():
+    if request.method == "GET" or request.method == "POST":
+        db = validate_user()
+
+        if not db:
+            abort(404)
+
+        user_storage = db[session.get("username")]
+
+        query_result = []
+
+        if user_storage.count_documents({}) == 0:
+            return jsonify({"result": None})
+        else:
+            for document in user_storage.find():
+                temp_dict = document
+                temp_dict["_id"] = str(temp_dict["_id"])
+                query_result.append(temp_dict)
+
+        return jsonify(query_result)
+    else:
+        abort(403)
+
+
+# Mongo user single document upload
+@app.route("/db/addONE/", methods=["POST"])
+@cross_origin()
+def add_document():
+    if request.method == "POST":
+        db = validate_user()
+
+        if not db:
+            abort(404)
+
+        user_storage = db[session.get("username")]
+
+        dict_for_mongo = request.get_json()
+
+        if dict_for_mongo is None:
+            abort(500)
+
+        if user_storage.insert_one(dict_for_mongo).acknowledged:
+            return jsonify({"result": True})
+        else:
+            abort(500)
+    else:
+        abort(402)
+
+
+# Mongo update document by id
+@app.route("/db/updateONE/", methods=["PUT"])
+@cross_origin()
+def update():
+    if request.method == "PUT":
+        db = validate_user()
+
+        if not db:
+            abort(404)
+
+        coll = db[session.get("username")]
+
+        dict_for_update = request.get_json()
+
+        dict_for_update["_id"] = ObjectId(dict_for_update["_id"])
+
+        pop_id = ""
+
+        try:
+            pop_id = dict_for_update.pop("_id")
+        except KeyError:
+            abort(500)
+
+        try:
+            coll.find_one_and_update({"_id": pop_id}, {"$set": dict_for_update})
+            return jsonify({"result": True})
+        except errors.PyMongoError:
+            abort(500)
+    else:
+        abort(402)
+
+
+# Mongo delete single document by id
+@app.route("/db/deleteONE/", methods=["DELETE"])
+@cross_origin()
+def delete_one():
+    if request.method == "DELETE":
+        db = validate_user()
+
+        if not db:
+            abort(404)
+
+        coll = db[session.get("username")]
+
+        value_to_delete = request.get_json()
+
+        value_to_delete["_id"] = ObjectId(value_to_delete["_id"])
+
+        if coll.find_one_and_delete(value_to_delete) is not None:
+            return jsonify({"result": True})
+        else:
+            abort(500)
+    else:
+        abort(402)
+
+
 # sw
 # @app.route("/sw.js")
 # def sw():
@@ -105,4 +228,4 @@ def user():
 
 # Page started in debug modify after, for Pycharm check the IDE launch config
 if __name__ == "__main__":
-    app.run("127.0.0.1", 5000, debug=True)
+    app.run("127.0.0.1", 5000)
--- static/JS/JSASYNCDATA/MongoDataHandler.js
+++ static/JS/JSASYNCDATA/MongoDataHandler.js
@@ -1,1 +1,33 @@
+function get_all_data() {
+    return $.ajax({
+        url: '/db/getALL/',
+        method: "GET",
+    })
+}
 
+function add_project(project_data) {
+    return $.ajax({
+        url: '/db/addONE/',
+        method: "POST",
+        data: JSON.stringify(project_data),
+        contentType: "application/json"
+    })
+}
+
+function update_proj(project_data) {
+    return $.ajax({
+        url: '/db/updateONE/',
+        method: "PUT",
+        data: JSON.stringify(project_data),
+        contentType: "application/json"
+    })
+}
+
+function delete_a_project(id) {
+    return $.ajax({
+        url: "/db/deleteONE/",
+        method: "DELETE",
+        data: JSON.stringify(id),
+        contentType: "application/json"
+    })
+}
