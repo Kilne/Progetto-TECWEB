d3dd3a7b42bffc3cf851680d648f226196c19197
Kilne <luca.maiuri86@gmail.com>
2021-11-23 16:09:16 +0100

    Test sulle DatabaseEntry.js e data_entry.py
    Signed-off-by: Kilne <luca.maiuri86@gmail.com>

--- static/JS/JSDATACLASS/DatabaseEntry.js
+++ static/JS/JSDATACLASS/DatabaseEntry.js
@@ -1,26 +1,21 @@
 export class DatabaseEntry {
-    #json_object = {
-        "name": "",
-        "obj": "",
-        "quantified_objective": 0,
-        "date": Object,
-        "time_left": 0,
-        "percentage_done": 0.0,
-        "step_total": 0,
-        "steps_left": 0,
-        "single_step_value": 0,
-        "is_finished": false,
-        "is_concrete": false
-    };
 
-    constructor(name_string, objective_string, objective_value_int, date_big_int, is_concrete_bool) {
-        this.#json_object.name = name_string;
-        this.#json_object.obj = objective_string;
-        this.#json_object.quantified_objective = objective_value_int;
-        this.#json_object.date = new Date(date_big_int);
-        this.#json_object.is_concrete = is_concrete_bool;
+    #name;
+    #obj;
+    #quantified_objective;
+    #obj_date;
+    #time_left;
+    #percentage_done;
+    #step_total;
+    #steps_left;
+    #single_step_value;
+    #is_finished;
+    #date_obj;
+
+    constructor() {
     }
 
-
+    complete_entry() {
+    }
 }
 
