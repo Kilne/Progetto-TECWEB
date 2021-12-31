f2df4ba83c6cccf3b893149683e7c0577f724f38
Kilne <luca.maiuri86@gmail.com>
2021-12-31 19:22:03 +0100

    Lavoro per invio dei form
    Signed-off-by: Kilne <luca.maiuri86@gmail.com>

--- static/JS/JSDATACLASS/DataValidation.js
+++ static/JS/JSDATACLASS/DataValidation.js
@@ -2,16 +2,18 @@ document.getElementById("Finalize").addEventListener("click", function (ev) {
     ev.preventDefault()
     //@TODO:validare e poi fare subimit all'url non mandare JSON poi ci pensa il backend
     let json_data = {};
-    json_data["project_name"] = document.getElementById("text-form").value
-    console.log("event fired")
-    console.log(json_data)
+    let form_aggreate = document.querySelectorAll(" div > input")
 
-
-    $.ajax({
-        url: "/finalize/",
-        method: "POST",
-        data: JSON.stringify(json_data),
-        content: "application/json"
+    form_aggreate.forEach(value => {
+        //    @TODO: manca solo prendere i valori dalla nodelist
     })
+
+
+    // $.ajax({
+    //     url: "/finalize/",
+    //     method: "POST",
+    //     data: JSON.stringify(json_data),
+    //     content: "application/json"
+    // })
 })
 
