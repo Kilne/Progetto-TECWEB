cc8048af3298e6ed87b399d3f0705b6cf5e5aa94
Kilne <luca.maiuri86@gmail.com>
2022-01-09 18:27:31 +0100

    Inizio develop data valid with regexp JS
    Signed-off-by: Kilne <luca.maiuri86@gmail.com>

--- static/JS/JSDATACLASS/DataValidation.js
+++ static/JS/JSDATACLASS/DataValidation.js
@@ -2,9 +2,25 @@ document.getElementById("Finalize").addEventListener("click", function (ev) {
     ev.preventDefault()
     let json_data = {};
     let input_forms = document.getElementsByClassName("form-control");
-    let radio_checked = $(".form-check-input")
+    let radio_checked = document.getElementsByClassName("form-check-input");
 
-    // @TODO ok CheckMover.js muove l'attrivuto checked per i radio , basta prender quello e dargli il value
+    for (const input_ele of input_forms) {
+        // @TODO: ci siamo quasi da affinare la reg exp e poi la data
+        if (input_ele.getAttribute("type") === "text") {
+            if (/([^a-z]|[^0-9])/.test(input_ele.value)) {
+                console.log("FARTS at" + input_ele.id);
+            } else {
+                json_data[input_ele.id] = input_ele.value;
+            }
+        }
+    }
+
+    for (const radioCheckedElement of radio_checked) {
+        if (radioCheckedElement.hasAttribute("checked")) {
+            json_data ["Prate"] = radioCheckedElement.value;
+        }
+    }
+
 
     $.ajax({
         url: "/finalize/",
